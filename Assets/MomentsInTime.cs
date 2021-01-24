4640d24897ab3a92047e49b91e7f91b395bf0e6e
Lauri Miettinen <lauri.miettinen90@gmail.com>
2021-01-24 11:07:40 +0200

    Documented new class

--- Assets/MomentsInTime.cs
+++ Assets/MomentsInTime.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Is used to store and retrieve the states and actions of objects and characters throughout moments in time.
+/// </summary>
 public class MomentsInTime
 {
 	List<ObjectInTime> momentsInTime = new List<ObjectInTime>();
