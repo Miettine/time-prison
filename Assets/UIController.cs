06c473c4fc22c988b4e13bdf5d898c74177af5f6
Lauri Miettinen <lauri.miettinen90@gmail.com>
2021-01-30 10:16:51 +0200

    Removed unneeded scripts

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIController : Singleton<UIController>
+public class UIController : MonoBehaviour
 {
 	Text TimeText;
 
