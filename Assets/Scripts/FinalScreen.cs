6a729fc356daddbc303078f06632dfbe53ef0592
miettine <lauri.miettinen90@gmail.com>
2025-12-21 20:00:29 +0200

    Exchange levels 7 and 8 to put the buggier level last. Make final screen. Improve title screen. Prepare builds

---
+++ Assets/Scripts/FinalScreen.cs
@@ -0,0 +1,46 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FinalScreen : MonoBehaviour
+{
+	private const string FullScreenButtonName = "FullScreenButton";
+
+	private const string RestartButtonName = "RestartGameButton";
+
+	private const string TitleScreenSceneName = "TitleScreen";
+
+	private TextMeshProUGUI fullScreenButtonText;
+
+	private void Awake()
+	{
+		var fullScreenButton = GameObject.Find(FullScreenButtonName).GetComponent<Button>();
+		fullScreenButton.onClick.AddListener(ToggleFullScreen);
+		fullScreenButtonText = fullScreenButton.transform.GetComponentInChildren<TextMeshProUGUI>();
+
+		GameObject.Find(RestartButtonName).GetComponent<Button>().onClick.AddListener(LoadTitleScreen);
+	}
+
+	private static void LoadTitleScreen()
+	{
+		SceneManager.LoadScene(TitleScreenSceneName);
+	}
+
+	private void Update()
+	{
+		UpdateFullScreenButtonText();
+		//The player can press the WebGL player's full screen button at any time.
+		//This is why I must update the full screen button's text every frame.
+	}
+
+	private static void ToggleFullScreen()
+	{
+		Screen.fullScreen = !Screen.fullScreen;
+	}
+
+	private void UpdateFullScreenButtonText()
+	{
+		fullScreenButtonText.text = Screen.fullScreen ? "Exit full screen" : "Go full screen";
+	}
+}
